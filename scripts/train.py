c5693b914fa732522dfa074eb09bd324546d60ea
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2024-07-06 23:42:19 +0330

    fix nn layer

--- scripts/train.py
+++ scripts/train.py
@@ -1,19 +1,20 @@
 import joblib
-import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
 
-X_train, X_test, y_train, y_test = joblib.load('data/preprocessed_data.pkl')
+X_train, X_test, y_train, y_test = joblib.load("data/preprocessed_data.pkl")
 
-model = Sequential([
-    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    Dense(64, activation='relu'),
-    Dense(1)
-])
+model = Sequential(
+    [
+        Input(shape=(8,)),
+        Dense(64, activation="relu", input_shape=(X_train.shape[1],)),
+        Dense(64, activation="relu"),
+        Dense(1),
+    ]
+)
 
-model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+model.compile(optimizer="adam", loss="mse", metrics=["mae"])
 
 model.fit(X_train, y_train, epochs=100, validation_split=0.1)
 
-model.save('model/concrete_model.h5')
-
+model.save("model/concrete_model.h5")
